ed636e34ba4e8f2b6747a911056ea2e8397c201e
francis-gomes-esq <figomesokoye@protonmail.com>
2024-01-14 18:47:14 +0000

    add pseudocode for fleshing out the logic

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,10 @@
+// HTML elements and variables initialization
+// Audio elements for correct and incorrect answer sounds
+// Timer and scoring variables
+// Function to start the quiz
+// End game if time runs out or all questions are answered
+// Function to display the next question
+// Function to check the user's answer
+// Function to end the game
+// Function to save the highscore
+// Event listener for starting the quiz
