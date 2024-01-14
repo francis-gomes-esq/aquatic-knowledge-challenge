363458e90a00d871ec3cfec4af0513a501ef3549
francis-gomes-esq <figomesokoye@protonmail.com>
2024-01-14 18:38:53 +0000

    add questions, options and correct answers to the file

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,74 @@
+// Array of marine biology-related questions with options and correct answers
+
+// Question 1
+
+var questions = [
+	{
+		question: 'What is the largest ocean on Earth?',
+		options: [
+			'Atlantic Ocean',
+			'Indian Ocean',
+			'Pacific Ocean',
+			'Arctic Ocean',
+		],
+		correctAnswer: 'Pacific Ocean',
+	},
+	// Question 2
+	{
+		question:
+			'Which marine animal is known for its ability to produce light through bioluminescence?',
+		options: ['Jellyfish', 'Shark', 'Octopus', 'Sea Horse'],
+		correctAnswer: 'Jellyfish',
+	},
+	// Question 3
+	{
+		question:
+			'What is the process by which marine organisms convert carbon dioxide into organic compounds using sunlight?',
+		options: ['Chemosynthesis', 'Respiration', 'Osmosis', 'Photosynthesis'],
+		correctAnswer: 'Photosynthesis',
+	},
+	// Question 4
+	{
+		question:
+			'Which of the following is a marine mammal known for its playful behavior and distinctive vocalizations?',
+		options: ['Seal', 'Walrus', 'Dolphin', 'Manatee'],
+		correctAnswer: 'Dolphin',
+	},
+	// Question 5
+	{
+		question:
+			'What is the largest mammal on Earth, and is often spotted in oceans across the globe?',
+		options: ['Blue Wale', 'Killer Whale', 'Dolphin', 'Sperm Whale'],
+		correctAnswer: 'Blue Wale',
+	},
+	// Question 6
+	{
+		question:
+			'Which of the following is a marine reptile that is often referred to as a "living fossil"',
+		options: ['Sea Turtle', 'Iguana', 'Plesiosaur', 'Crocodile'],
+		correctAnswer: 'Plesiosaur',
+	},
+	// Question 7
+	{
+		question:
+			'What is the term for the phenomenon where warm ocean currents meet cold currents, creating nutrient-rich areas that support a diverse marine ecosystem?',
+		options: [
+			'Oceanic Upwelling',
+			'Coral Bleaching',
+			'El Niño',
+			'Thermohaline Circulation',
+		],
+		correctAnswer: 'Oceanic Upwelling',
+	},
+	// Question 8
+	{
+		question: "What is the primary function of a fish's swim bladder?",
+		options: [
+			'Buoyancy control',
+			'Digestion',
+			'Temperature regulation',
+			'Sonar navigation',
+		],
+		correctAnswer: 'Buoyancy control',
+	},
+]
